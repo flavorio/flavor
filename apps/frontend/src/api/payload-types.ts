0ad9008c406151b4364ac367931282b27c657986
nxjniexiao <30365414+nxjniexiao@users.noreply.github.com>
2024-08-25 21:29:32 +0800

    feat: remove apps/frontend

--- apps/frontend/src/api/payload-types.ts
+++
@@ -1,3 +0,0 @@
-type PayloadWithId = {
-  id: string;
-};
--- apps/frontend/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './styled';
-export * from './sidebar';
-export * from './toggle-sidebar';
-export * from './user/user-avatar';
